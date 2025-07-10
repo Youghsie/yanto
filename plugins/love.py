ebf2175a9f8d5bb88aa7d9f86b62c776fdb30fa9
Itoshie Ryn. <randaka98@gmail.com>
2025-07-11 01:57:52 +0700

    Ryn

--- plugins/love.py
+++ plugins/love.py
@@ -1,7 +1,5 @@
 import random
-
 from pyrogram import filters
-
 from DanteMusic import app
 
 
@@ -30,21 +28,20 @@ def get_random_message(love_percentage):
                 "Destined to be together. Congratulations!",
             ]
         )
-
-
+        
 @app.on_message(filters.command("love", prefixes="/"))
 def love_command(client, message):
     command, *args = message.text.split(" ")
     if len(args) >= 2:
         name1 = args[0].strip()
         name2 = args[1].strip()
-
+        
         love_percentage = random.randint(10, 100)
         love_message = get_random_message(love_percentage)
 
         response = f"{name1}💕 + {name2}💕 = {love_percentage}%\n\n{love_message}"
     else:
-        response = "Please enter two names after /love command."
+        response = "Silakan masukkan dua nama setelah perintah /love."
     app.send_message(message.chat.id, response)
 
 __MODULE__ = "Love"
